c92d668dae058dfb61af90cceaea43132f31017a
Anthony Shaw <anthony.p.shaw@gmail.com>
2019-01-01 15:36:18 +1100

    add old style urls

--- django-multi-environment/azuredemo/azuredemo/urls.py
+++ django-multi-environment/azuredemo/azuredemo/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import url, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('demo/', include('demo.urls'))
+    url(r'^admin/', admin.site.urls),
+    url(r'^demo/', include('demo.urls'))
 ]
